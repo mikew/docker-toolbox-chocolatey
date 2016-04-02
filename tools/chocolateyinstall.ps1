4ac4b1e463d5fe157f564c259980b146fdda71da
Mike Wyatt <wyatt.mike@gmail.com>
2016-04-02 16:30:39 -0300

    initial

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'docker-toolbox'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://github.com/docker/toolbox/releases/download/v1.10.3/DockerToolbox-1.10.3.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+  softwareName  = 'Docker Toolbox*'
+  checksum      = ''
+  checksumType  = 'md5'
+  checksum64    = ''
+  checksumType64= 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'docker-toolbox'
+$softwareName = 'Docker Toolbox*'
+$installerType = 'EXE'
+
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+$uninstalled = $false
+$local_key     = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key   = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*'
+$machine_key6432 = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+
+$key = Get-ItemProperty -Path @($machine_key6432,$machine_key, $local_key) `
+                        -ErrorAction SilentlyContinue `
+         | ? { $_.DisplayName -like "$softwareName" }
+$file = $key.UninstallString
+
+Uninstall-ChocolateyPackage -PackageName $packageName `
+                            -FileType $installerType `
+                            -SilentArgs "$silentArgs" `
+                            -ValidExitCodes $validExitCodes `
+                            -File "$file"
