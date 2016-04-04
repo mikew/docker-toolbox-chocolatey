f82fb901302e2e43c236f8dd5f4628bee32a4f2c
Mike Wyatt <wyatt.mike@gmail.com>
2016-04-03 23:37:49 -0300

    add support for /SkipVirtualBox, /SkipKitematic, and /SkipGit params

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,13 +3,29 @@ $packageName= 'docker-toolbox'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url64      = 'https://github.com/docker/toolbox/releases/download/v1.10.3/DockerToolbox-1.10.3.exe'
 
+$params = $env:chocolateyPackageParameters
+$components = 'Docker,DockerMachine,DockerCompose'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+
+if (-Not ("$params" -match "/SkipVirtualBox")) {
+  $components += ",VirtualBox"
+}
+
+if (-Not ("$params" -match "/SkipKitematic")) {
+  $components += ",Kitematic"
+}
+
+if (-Not ("$params" -match "/SkipGit")) {
+  $components += ",Git"
+}
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
   url64bit      = $url64
-  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  silentArgs    = "/COMPONENTS='$components' $silentArgs"
   validExitCodes= @(0)
   softwareName  = 'Docker Toolbox*'
   checksum      = ''
